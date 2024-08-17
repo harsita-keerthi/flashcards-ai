a6e2793df4b722124a04cb92f6d6e37226721f9b
LinCodeVibe <hsiaolin29@gmail.com>
2024-08-16 18:14:39 -0700

    displaying the page

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+'use client';
+
 import Image from 'next/image';
 import getStripe from '@/utils/get-stripe';
 
@@ -6,33 +8,35 @@ import { AppBar, Button, Container, Toolbar, Typography, Box } from '@mui/materi
 import Head from 'next/head';
 
 export default function Home() {
-	<Container maxWidth='100v'>
-		<Head>
-			<title>Flashcard SaaS</title>
-			<meta name='description' content='Create flashcard from your text' />
-		</Head>
+	return (
+		<Container maxWidth='100v'>
+			<Head>
+				<title>Flashcard SaaS</title>
+				<meta name='description' content='Create flashcard from your text' />
+			</Head>
 
-		<AppBar position='static'>
-			<Toolbar>
-				<Typography variant='h6' stype={{ flexGrow: 1 }}>
-					Flashcard SaaS
-				</Typography>
-				<SignedOut>
-					<Button color='inherit'>Login</Button>
-					<Button color='inherit'>Sign Up</Button>
-				</SignedOut>
+			<AppBar position='static'>
+				<Toolbar>
+					<Typography variant='h6' stype={{ flexGrow: 1 }}>
+						Flashcard SaaS
+					</Typography>
+					<SignedOut>
+						<Button color='inherit'>Login</Button>
+						<Button color='inherit'>Sign Up</Button>
+					</SignedOut>
 
-				<SignedIn>
-					<UserButton />
-				</SignedIn>
-			</Toolbar>
-		</AppBar>
-		<Box>
-			<Typography variant='h2'> Welcome to Flashcard SaaS</Typography>
-			<Typography variant='h5'> The easist way to make flashcards from your text</Typography>
-			<Button variant='contained' color='primary' sx={{ mt: 2 }}>
-				GET STARTED
-			</Button>
-		</Box>
-	</Container>;
+					<SignedIn>
+						<UserButton />
+					</SignedIn>
+				</Toolbar>
+			</AppBar>
+			<Box>
+				<Typography variant='h2'> Welcome to Flashcard SaaS</Typography>
+				<Typography variant='h5'> The easist way to make flashcards from your text</Typography>
+				<Button variant='contained' color='primary' sx={{ mt: 2 }}>
+					GET STARTED
+				</Button>
+			</Box>
+		</Container>
+	);
 }
